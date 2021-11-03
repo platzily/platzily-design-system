f38d7417a43082388c571bfc3e291b295238e6eb
Omar Flores Grimontt <omar.flores@rappi.com>
2021-11-02 22:33:31 -0500

    feature-21 Created Link icon to let user associate it links with an icon

--- .eslintrc.js
+++ .eslintrc.js
@@ -76,6 +76,7 @@ module.exports = {
     'react/state-in-constructor': 'off',
     'react/static-property-placement': 'off',
     'react/react-in-jsx-scope': 'off',
+    'jsx-a11y/anchor-is-valid': 'off'
   },
   settings: {
     'import/resolver': {
---
+++ packages/docs/components/icons/Icons.js
@@ -0,0 +1,10 @@
+import { Fragment } from 'react';
+import { Link } from '@platzily-ui/icons';
+
+export default function Icons() {
+  return (
+    <Fragment>
+      <Link width="3rem" height="3rem"/>
+    </Fragment>
+  );
+}
