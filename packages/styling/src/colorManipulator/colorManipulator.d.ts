faf312ce2b62de18847f6305f2f61b8eb54935ee
Kevin Farid <kpelaez75@gmail.com>
2021-11-05 15:46:11 -0500

    feature-27 delete interface because it is not used

--- packages/styling/src/colorManipulator/colorManipulator.d.ts
+++ packages/styling/src/colorManipulator/colorManipulator.d.ts
@@ -1,8 +1,1 @@
-export type ColorFormat = 'rgb' | 'rgba' | 'hsl' | 'hsla';
-
-export interface ColorObject {
-  type: ColorFormat;
-  values: [number, number, number] | [number, number, number, number];
-  colorSpace?: 'srgb' | 'display-p3' | 'a98-rgb' | 'prophoto-rgb' | 'rec-2020';
-}
 export function alpha(color: string, value: number): string;
