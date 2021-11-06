03a9e2fcbe3127ee665fd0d649bf2cd8a184a7c9
Kevin Farid <kpelaez75@gmail.com>
2021-11-05 23:21:15 -0500

    feature-15 add cx to combine className in Buttons

--- packages/components/src/Button/Button.js
+++ packages/components/src/Button/Button.js
@@ -2,10 +2,6 @@ import { forwardRef } from 'react';
 import { createStyleSheet, alpha as convertRgba } from '@platzily-ui/styling';
 import { cx } from '@emotion/css';
 
-const selectClass = (props) => {
-  return props.variant === 'secondary' ? 'secondaryButton' : 'primaryButton';
-};
-
 const useStyleSheet = createStyleSheet(
   (theme, props) => {
     return {
@@ -20,16 +16,16 @@ const useStyleSheet = createStyleSheet(
         color: theme.palette.primary.main,
         '&:hover': {
           color: theme.palette.text.light,
-          backgroundColor: convertRgba(props.color || theme.palette.secondary.main, 0.6)
+          backgroundColor: convertRgba(props.color || theme.palette.secondary.main, 0.6),
         },
         '&:focus': {
-          backgroundColor: convertRgba(props.color || theme.palette.secondary.main, 0.6)
+          backgroundColor: convertRgba(props.color || theme.palette.secondary.main, 0.6),
         },
         '&:disabled': {
           backgroundColor: theme.palette.text.disabled,
           color: theme.palette.text.secondary,
           PointerEvents: 'none',
-          cursor: 'default'
+          cursor: 'default',
         },
       },
       secondaryButton: {
@@ -44,18 +40,18 @@ const useStyleSheet = createStyleSheet(
         fontSize: '20px',
         color: props.color || theme.palette.primary.main,
         '&:hover': {
-          backgroundColor: convertRgba(theme.palette.secondary.main, 0.6),
+          backgroundColor: convertRgba(props.color ||theme.palette.secondary.main, 0.6),
         },
         '&:focus': {
           backgroundColor: theme.palette.text.light,
-          borderColor: theme.palette.secondary.main,
+          borderColor: props.color || theme.palette.secondary.main,
         },
         '&:disabled': {
           PointerEvents: 'none',
           backgroundColor: 'transparent',
           color: theme.palette.text.disabled,
           borderColor: theme.palette.text.disabled,
-          cursor: 'default'
+          cursor: 'default',
         },
       },
     };
@@ -63,10 +59,25 @@ const useStyleSheet = createStyleSheet(
   { key: 'button' },
 );
 
+const selectClass = (props) => {
+  return props.variant === 'secondary' ? 'secondaryButton' : 'primaryButton';
+};
+
+const combiningClassName = (classes, props) => {
+  return cx(classes, props.className);
+};
+
 const Button = forwardRef(function Button(props, ref) {
   const { classes } = useStyleSheet(props);
   const buttonStyle = selectClass(props);
-  return <button ref={ref} type="button" {...props} className={cx(classes[buttonStyle], props)} />;
+  return (
+    <button
+      ref={ref}
+      type="button"
+      {...props}
+      className={combiningClassName(classes[buttonStyle], props)}
+    />
+  );
 });
 
 export default Button;
--- packages/docs/components/Button/Button.js
+++ packages/docs/components/Button/Button.js
@@ -1,11 +1,23 @@
 import { Button } from '@platzily-ui/components';
-import { ThemeProvider } from '@platzily-ui/styling';
+import { ThemeProvider, createStyleSheet } from '@platzily-ui/styling';
 import { defaultTheme } from './theme';
 
+const useStyleSheet = createStyleSheet(
+  () => {
+    return {
+      box: {
+        margin: '10px',
+      },
+    };
+  },
+  { key: 'margin' },
+);
+
 export default function ButtonComponent(props) {
+  const { classes } = useStyleSheet();
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button type="button" style={{ margin: '10px' }} {...props}>
+      <Button type="button" className={classes.box} {...props}>
         {props.children}
       </Button>
     </ThemeProvider>
