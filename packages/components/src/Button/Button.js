5efb2fce0913cc5cd0edbb3f065bf602aa7463dd
Kevin Farid <kpelaez75@gmail.com>
2021-11-07 22:01:19 -0500

    feature-15 add Text Button to the text actions

--- packages/components/src/Button/Button.js
+++ packages/components/src/Button/Button.js
@@ -1,18 +1,23 @@
 import { forwardRef } from 'react';
 import { createStyleSheet, alpha as convertRgba } from '@platzily-ui/styling';
-import { cx } from '@emotion/css';
+import { PropTypes } from 'prop-types';
 
 const useStyleSheet = createStyleSheet(
   (theme) => {
     return {
-      primary: {
+      contained: {
         backgroundColor: theme.palette.secondary.main,
         borderRadius: 5,
-        width: 210,
-        height: 40,
+        paddingTop: 10,
+        paddingLeft: 15,
+        paddingBottom: 9,
+        paddingRight: 15,
         textAlign: 'center',
         fontWeight: 500,
-        fontSize: '1.25rem',
+        fontSize: '0.875rem',
+        textTransform: 'uppercase',
+        lineHeight: '1rem',
+        letterSpacing: '0.02em',
         color: theme.palette.primary.main,
         '&:hover': {
           color: theme.palette.text.light,
@@ -27,17 +32,22 @@ const useStyleSheet = createStyleSheet(
           cursor: 'not-allowed',
         },
       },
-      secondary: {
+      outlined: {
         backgroundColor: 'transparent',
         borderRadius: 5,
         borderWidth: 1.5,
         borderStyle: 'solid',
         borderColor: theme.palette.primary.main,
-        width: 210,
-        height: 40,
+        paddingTop: 10,
+        paddingLeft: 15,
+        paddingBottom: 9,
+        paddingRight: 15,
         textAlign: 'center',
         fontWeight: 500,
-        fontSize: '1.25rem',
+        fontSize: '0.875rem',
+        textTransform: 'uppercase',
+        lineHeight: '1rem',
+        letterSpacing: '0.02em',
         color: theme.palette.primary.main,
         '&:hover': {
           backgroundColor: convertRgba(theme.palette.secondary.main, 0.6),
@@ -47,31 +57,65 @@ const useStyleSheet = createStyleSheet(
           borderColor: theme.palette.secondary.main,
         },
         '&:disabled': {
-          cursor: 'not-allowed',
           backgroundColor: 'transparent',
           color: theme.palette.text.disabled,
           borderColor: theme.palette.text.disabled,
+          cursor: 'not-allowed'
         },
       },
+      text: {
+        backgroundColor: 'transparent',
+        textAlign: 'center',
+        paddingTop: 10,
+        paddingLeft: 15,
+        paddingBottom: 9,
+        paddingRight: 15,
+        fontWeight: 700,
+        fontSize: '1rem',
+        lineHeight: '1rem',
+        letterSpacing: '0.02em',
+        textTransform: 'uppercase',
+        textDecoration: 'underline',
+        textDecorationColor: theme.palette.secondary.main,
+        color: theme.palette.secondary.main,
+        '&:hover': {
+          color: theme.palette.info.main,
+          textDecorationColor: theme.palette.info.main,
+        },
+        '&:focus': {
+          color: theme.palette.primary.main,
+          textDecorationColor: theme.palette.primary.main
+        },
+        '&:disabled': {
+          color: theme.palette.text.disabled,
+          textDecorationColor: theme.palette.text.disabled
+        }
+      }
     };
   },
   { key: 'button' },
 );
 
-const combiningClassName = (classes, props) => {
-  return cx(classes[props.variant || 'primary'], props.className);
-};
-
 const Button = forwardRef(function Button(props, ref) {
-  const { classes } = useStyleSheet(props);
+  const { variant, className, ...otherProps } = props;
+  const { classes, cx } = useStyleSheet(props);
   return (
     <button
       ref={ref}
       type="button"
-      {...props}
-      className={combiningClassName(classes, props)}
+      {...otherProps}
+      className={cx(classes, className)}
     />
   );
 });
+
+Button.propTypes = {
+  className: PropTypes.string,
+  variant: PropTypes.oneOf(['contained', 'outlined', 'text']),
+};
+
+Button.defaultProps = {
+  variant: 'contained',
+};
 
 export default Button;
