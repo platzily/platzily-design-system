4cb15bf4fe6f5cc61fb794c044c9c050b1ab97e8
Omar Flores Grimontt <omar.flores@rappi.com>
2021-11-08 10:06:36 -0500

    feature-21 Started Icons component in docs to documentate Icon component.

--- packages/docs/components/icons/Icons.js
+++ packages/docs/components/icons/Icons.js
@@ -1,10 +1,47 @@
-import { Fragment } from 'react';
+import { createStyleSheet } from '@platzily-ui/styling';
 import { Link } from '@platzily-ui/icons';
 
+const icons = [
+  {
+    id: 0,
+    Icon: Link,
+    name: 'Link',
+    path: "import { Link } from '@platzily-ui/icons';"
+  }
+];
+
+const useStyleSheet = createStyleSheet(() => ({
+  root: {
+    backgroundColor: '#f1f1f1',
+    borderRadius: 8,
+    marginTop: 12,
+    padding: 8
+  },
+  input: {
+    height: '2rem',
+    borderRadius: 4
+  },
+  inputContainer: {
+    margin: '12px 0'
+  }
+}));
+
 export default function Icons() {
+  const { classes } = useStyleSheet();
+
   return (
-    <Fragment>
-      <Link width="3rem" height="3rem"/>
-    </Fragment>
+    <div className={classes.root}>
+      <div className={classes.inputContainer}>
+        <input className={classes.input}/>
+      </div>
+      <div>
+        {icons.map(icon => (
+          <div key={icon.id}>
+            <icon.Icon/>
+            <span>{icon.name}</span>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
