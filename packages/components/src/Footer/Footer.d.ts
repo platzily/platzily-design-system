0115be6f774cc0f2878b964965d4b7e1329a1431
danielvaldivv <dvaldiviesomerino.drvm@gmail.com>
2021-11-17 15:46:08 -0500

    feature-53 created Footer Component

---
+++ packages/components/src/Footer/Footer.d.ts
@@ -0,0 +1,4 @@
+import { HTMLProps } from 'react';
+
+export interface FooterProps extends HTMLProps<HTMLHeadingElement> {}
+export default function Footer(props: FooterProps): JSX.Element;
---
+++ packages/components/src/Footer/Footer.js
@@ -0,0 +1,43 @@
+import { forwardRef } from 'react';
+import { createStyleSheet } from '@platzily-ui/styling';
+import PropTypes from 'prop-types';
+
+const useStyleSheet = createStyleSheet(
+  (theme, { bgColor, position }) => ({
+    footer: {
+      backgroundColor: bgColor || theme.palette.neutral.light,
+      position,
+      width: '100%',
+      height: 60,
+      padding: 15,
+      margin: 0,
+    },
+
+    footerText: {},
+  }),
+  { key: 'footerWrapper' },
+);
+
+const Footer = forwardRef(function Footer(props, ref) {
+  const { bgColor, position, className, ...otherProps } = props;
+  const { classes, cx } = useStyleSheet(props);
+
+  return <footer
+    ref={ref}
+    className={cx(classes.footer, className)}
+    {...otherProps}
+  />
+  ;
+});
+
+Footer.propTypes = {
+  bgColor: PropTypes.string,
+  className: PropTypes.string,
+  position: PropTypes.oneOf(['static', 'relative', 'absolute', 'fixed', 'sticky', 'unset']),
+};
+
+Footer.defaultProps = {
+  position: 'fixed',
+};
+
+export default Footer;
