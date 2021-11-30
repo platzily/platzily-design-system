075ac38dbb4225b9914b38e84f10094d979e47f3
Kevin Farid <kpelaez75@gmail.com>
2021-11-30 00:29:10 -0500

    feature-42 add active prop to active status

--- packages/components/src/ListItem/ListItem.js
+++ packages/components/src/ListItem/ListItem.js
@@ -10,6 +10,11 @@ const useStyleSheet = createStyleSheet((theme) => {
       padding: theme.spacing(2),
       color: theme.palette.primary.main,
       lineHeight: '1rem',
+    },
+    active: {
+      fontWeight: 'bold',
+    },
+    noActive: {
       '&:hover': {
         textDecoration: 'underline',
       },
@@ -27,12 +32,24 @@ const useStyleSheet = createStyleSheet((theme) => {
 
 const ListItem = forwardRef(function ListItem(props, ref) {
   const { classes, cx } = useStyleSheet(props);
-  const { className, ...otherProps } = props;
-  return <button type="button" {...otherProps} ref={ref} className={cx(classes.root, className)} />;
+  const { className, status, ...otherProps } = props;
+  return (
+    <button
+      type="button"
+      {...otherProps}
+      ref={ref}
+      className={cx(classes.root, classes[status], className)}
+    />
+  );
 });
 
 ListItem.propTypes = {
   className: PropTypes.string,
+  status: PropTypes.string,
+};
+
+ListItem.defaultProps = {
+  status: 'noActive',
 };
 
 export default ListItem;
